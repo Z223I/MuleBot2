2d8cbf7cceec37705e9a6a95c50a3587b96692ae
Z223I <BruceWilsonPH@yahoo.com.ph>
2018-02-15 11:49:43 -0600

    Building Accessory class and its tests.

--- MuleBot/Accessory.py
+++ MuleBot/Accessory.py
@@ -1,18 +1,32 @@
 #/usr/bin/python
 
+#import threading
+
 class Accessory:
     """Accessory """
 
     def __init__(self):
+        self._running = True
         self.time_on = 2
         self.time_off = 4
 
-    def water_pump(self):
-        pass
+    def terminate(self):
+        self._running = False
         
+    def is_running(self):
+        return self._running
+
+    def x(self):
+        y = 2
+
     def _w_p_init(self):
         pass
         
     def _w_p_loop(self):
         pass
+    
+    def water_pump(self):
+        self._w_p_init()
         
+        while self.is_running():
+            self._w_p_loop()
--- test/test_Accessory.py
+++ test/test_Accessory.py
@@ -21,11 +21,28 @@ class TestAccessory(unittest.TestCase):
         pass
 
     def test___init__(self):
+        self.assertTrue(self.testAccessory._running)
         self.assertEqual(self.testAccessory.time_on, 2)
         self.assertEqual(self.testAccessory.time_off, 4)
 
-    def test_water_pump(self):
-        pass
+    def test_terminate(self):
+        self.testAccessory.terminate()
+        self.assertFalse(self.testAccessory._running)
+        
+    def test_is_running(self):
+        running = self.testAccessory.is_running()
+        self.assertTrue(running)
+
+    @patch('Accessory.Accessory.is_running')
+    @patch('Accessory.Accessory._w_p_loop')
+    @patch('Accessory.Accessory._w_p_init')
+    def test_water_pump(self, mock__w_p_init, mock__w_p_loop, mock_is_running):
+        mock_is_running.side_effect = [True, False]
+        
+        self.testAccessory.water_pump()
+        self.assertTrue(mock__w_p_init.called)
+        self.assertTrue(mock__w_p_loop.called)
+        self.assertTrue(mock_is_running.called)
         
     def test__w_p_init(self):
         pass
